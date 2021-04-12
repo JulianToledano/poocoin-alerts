27d9a282a2a6914662ed5f0f0accd11353e8a4ba
rrossilli55 <rrossilli55@gmail.com>
2021-04-11 22:11:22 -0400

    changes

--- main.go
+++ main.go
@@ -6,52 +6,66 @@ import (
 	"log"
 	"net/http"
 	"net/smtp"
+	"time"
 )
 
 func main() {
-	r, err := http.Get("https://poocoin.app/whitelist1-tokens.json")
-	if err != nil {
-		// handle error
+
+	for {
+		fmt.Println("fetching updates")
+
+		r, err := http.Get("https://poocoin.app/whitelist1-tokens.json")
+		if err != nil {
+			// handle error
+		}
+		defer r.Body.Close()
+
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		response := string(body)
+
+		lastResponse, err := ioutil.ReadFile("./last_response.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if response != string(lastResponse) {
+			ioutil.WriteFile("./last_response.txt", []byte(response), 0644)
+			//email
+			fmt.Println("change")
+			sendEmail()
+		} else {
+			// do nothing
+			fmt.Println("no change")
+		}
+
+		time.Sleep(time.Minute)
 	}
-	defer r.Body.Close()
-
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	response := string(body)
-
-	lastResponse, err := ioutil.ReadFile("./last_response.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if response != string(lastResponse) {
-		ioutil.WriteFile("./last_response.txt", []byte(response), 0644)
-		//email
-		fmt.Println("change")
-	} else {
-		// do nothing
-		fmt.Println("no change")
-		sendEmail()
-	}
-
-	// fmt.Println(string(body))
 }
 
 func sendEmail() {
-	email := "some_email"
-	pass := "some_password"
-	host := "smtp.mailtrap.io"
+	email := "talos.tester.1@gmail.com"
+	pass := "Tester123!"
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587"
+	message := []byte("Poocoin updated their feature list!")
 
-	auth := smtp.PlainAuth("", email, pass, host)
-
-	// Here we do it all: connect to our server, set up a message and send it
-	to := []string{email}
-	msg := []byte("poocoin listed new feature coins")
-	err := smtp.SendMail("smtp.mailtrap.io:25", auth, "piotr@mailtrap.io", to, msg)
-	if err != nil {
-		log.Fatal(err)
+	// add recipient emails here
+	to := []string{
+		"talos.tester.1@gmail.com",
+		"7rossilli7@gmail.com",
+		"rrossilli55@gmail.com",
 	}
+
+	auth := smtp.PlainAuth("", email, pass, smtpHost)
+
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, email, to, message)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Email Sent Successfully!")
 }
